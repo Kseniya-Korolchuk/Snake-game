3fc92a55d67b4556eba8fa15797140d88c92e243
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-02-22 02:36:28 +0300

    feat: add sounds and restart

--- random-game/index.js
+++ random-game/index.js
@@ -1,15 +1,26 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+const gameFin = document.querySelector('.game-over');
+const scoreBox = document.querySelector('.game-data');
+const restart = document.querySelector('.restart');
 let score = 0;
 let rank = 1;
-const scoreBox = document.querySelector('.game-data');
-let time = 300;
 let snake = [];
 let game;
+let time = 300;
 
-game = setInterval(drawGame, time);
+
+//ПЕРЕЗАГРУЖАЕМ СТРАНИЦУ КНОПКОЙ
+restart.addEventListener('click', function(event) {
+    if(event){
+        window.location.reload();
+    }
+});
+
 
 //ОБНОВЛЯЕМ ПОЛЕ ИГРЫ
+game = setInterval(drawGame, time);
+
 function drawGame() {
     ctx.clearRect(0, 0, 540, 540);
     let snakeX = snake[0].x;
@@ -24,6 +35,7 @@ function drawGame() {
         clearInterval(game);
         scoreTable.unshift(score);
         saveScore();
+        win();
     }
     drawTarget();
     drawSnake();
@@ -32,8 +44,8 @@ function drawGame() {
     move();
     eat();
     scoreBox.textContent = `SCORE: ${score} RANK: ${rank}`;
-    
 }
+
 
 //ОТРИСОВЫВАЕМ ЗМЕЮ
 function drawSnake() {
@@ -56,6 +68,9 @@ function move() {
 //ЗМЕЯ ЕСТ
 function eat() {
     if (snake[0].x == target.x && snake[0].y == target.y) {
+        let audio = new Audio();
+        audio.src = './assets/mp3/getItem.mp3'
+        audio.play();
         let food = '';
         snake.push(food);
         target = {
@@ -68,6 +83,8 @@ function eat() {
     }
 }
 
+
+//УВЕЛИЧИВАЕМ СКОРОСТЬ
 function riseSpeed(scr) {
     if(score > 0 && score % 5 == 0) {
         time -= 50;
@@ -76,18 +93,18 @@ function riseSpeed(scr) {
     }
 }
     
-//ВЕШАЕМ ОБРАБОТЧИК СОБЫТИЙ
+//ВЕШАЕМ СЛУШАТЕЛЯ
 document.addEventListener('keydown', direction);
 let dir;
 function direction(event) {
     
-    if (event.keyCode == 37)
+    if (event.keyCode == 37 && dir !== 'right')
     dir = 'left';
-    else if (event.keyCode == 38) 
+    else if (event.keyCode == 38 && dir !== 'down') 
     dir = 'up';
-    else if (event.keyCode == 39) 
+    else if (event.keyCode == 39 && dir !== 'left') 
     dir = 'right';
-    else if (event.keyCode == 40) 
+    else if (event.keyCode == 40 && dir !== 'up') 
     dir = 'down';
 }
 
@@ -108,19 +125,24 @@ y: Math.floor((Math.random() * 26 + 1)) * square
 
 //ПРОВЕРЯЕМ, НЕ КУСАЕТ ЛИ ЗМЕЯ СЕБЯ
 function bitesItself(firstEl, arr) {
-    console.log(firstEl)
         for(let i = 1; i < arr.length; i++) {
-            if(firstEl.x == arr[i].x && firstEl.y == arr[i].y)
-                clearInterval(game);
+            if(firstEl.x == arr[i].x && firstEl.y == arr[i].y) {
+                scoreTable.unshift(score);
                 saveScore();
+                clearInterval(game);
+                loose();
+            }  
         }
 }
+
 
 //ПРОВЕРЯЕМ, НЕ УПЕРЛАСЬ ЛИ ЗМЕЯ В СТЕНКУ
 function isOnBoard(x, y){
     if(x < 0 || x > 26 * square || y < 0 || y > 26 * square ) {
-        clearInterval(game)
+        scoreTable.unshift(score);
         saveScore();
+        clearInterval(game);
+        loose();
     }
 }
 
@@ -133,7 +155,7 @@ snake[0] = {
 
 
 
-//СКЛАДЫВАЕМ РЕЗУЛЬТАТЫ В МАССИВ
+//ВЫВОДИМ РЕЗУЛЬТАТЫ В ТОР 10
 let scoreTable;
 function setScoreTable() {
     if(!localStorage.getItem('results')){
@@ -152,7 +174,25 @@ function setScoreTable() {
 }
 setScoreTable();
 
-//ЗАПОЛНЯЕМ ТАБЛИЦУ РЕЗУЛЬТАТОВ
+
+//ПОБЕДА
+function win(){
+    let audio = new Audio();
+    audio.src = './assets/mp3/win.mp3'
+    audio.play();
+    gameFin.textContent = 'WIN!';
+    gameFin.classList.add('active');
+}
+
+
+//ПОРАЖЕНИЕ
+function loose() {
+    let audio = new Audio();
+    audio.src = './assets/mp3/loose.mp3'
+    audio.play();
+    gameFin.textContent = 'Game\nover';
+    gameFin.classList.add('active');
+}
 
 
 //СОХРАНЯЕМ СЧЕТ
